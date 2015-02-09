f461026188f26cd028335557f3ae3014616f0b51
smiller393 <smiller393@gmail.com>
2015-02-08 23:32:52 -0500

    added zooming/panning to the Gantt core file Note: Example is broken right now

--- gantt-chart-d3.js
+++ gantt-chart-d3.js
@@ -8,18 +8,26 @@ d3.gantt = function() {
     var FIXED_TIME_DOMAIN_MODE = "fixed";
     
     var margin = {
-	top : 20,
-	right : 40,
-	bottom : 20,
-	left : 150
+		top : 20,
+		right : 40,
+		bottom : 20,
+		left : 150
     };
     var timeDomainStart = d3.time.day.offset(new Date(),-3);
     var timeDomainEnd = d3.time.hour.offset(new Date(),+3);
     var timeDomainMode = FIT_TIME_DOMAIN_MODE;// fixed or fit
     var taskTypes = [];
-    var taskStatus = [];
-    var height = document.body.clientHeight - margin.top - margin.bottom-5;
-    var width = document.body.clientWidth - margin.right - margin.left-5;
+    var eventStyles = [];
+	var eventStyleCount = function(){
+		return eventStyles.length;
+	}
+    var height = 800 - margin.top - margin.bottom-5;
+    var width = 1800 - margin.right - margin.left-5;
+
+	var currentViewBeginTime;
+	var currentViewEndTime;
+	currentViewBeginTime = d3.time.day.offset(getEndDate(), -1);
+	currentViewEndTime = getEndDate();
 
     var tickFormat = "%H:%M";
 
@@ -41,26 +49,26 @@ d3.gantt = function() {
     var yAxis = d3.svg.axis().scale(y).orient("left").tickSize(0);
 
     var initTimeDomain = function(tasks) {
-	if (timeDomainMode === FIT_TIME_DOMAIN_MODE) {
-	    if (tasks === undefined || tasks.length < 1) {
-		timeDomainStart = d3.time.day.offset(new Date(), -3);
-		timeDomainEnd = d3.time.hour.offset(new Date(), +3);
-		return;
-	    }
-	    tasks.sort(function(a, b) {
-		return a.endDate - b.endDate;
-	    });
-	    timeDomainEnd = tasks[tasks.length - 1].endDate;
-	    tasks.sort(function(a, b) {
-		return a.startDate - b.startDate;
-	    });
-	    timeDomainStart = tasks[0].startDate;
-	}
+		if (timeDomainMode === FIT_TIME_DOMAIN_MODE) {
+			if (tasks === undefined || tasks.length < 1) {
+			timeDomainStart = d3.time.day.offset(new Date(), -3);
+			timeDomainEnd = d3.time.hour.offset(new Date(), +3);
+			return;
+			}
+			tasks.sort(function(a, b) {
+			return a.endDate - b.endDate;
+			});
+			timeDomainEnd = tasks[tasks.length - 1].endDate;
+			tasks.sort(function(a, b) {
+			return a.startDate - b.startDate;
+			});
+			timeDomainStart = tasks[0].startDate;
+		}
     };
 
     var initAxis = function() {
 		x = d3.time.scale().domain([ timeDomainStart, timeDomainEnd ]).range([ 0, width ]).clamp(true);
-		y = d3.scale.ordinal().domain(taskTypes).rangeRoundBands([ 0, height - margin.top - margin.bottom ], .1);
+		y = d3.scale.ordinal().domain(taskTypes).rangeRoundBands([ 0, (height) - margin.top - margin.bottom ],.2);
 		xAxis = d3.svg.axis().scale(x).orient("bottom").tickFormat(d3.time.format(tickFormat)).tickSubdivide(true)
 			.tickSize(8).tickPadding(8);
 
@@ -89,8 +97,10 @@ d3.gantt = function() {
 			 .attr("rx", 5)
 				 .attr("ry", 5)
 			 .attr("class", function(d){
-				 if(taskStatus[d.status] == null){ return "bar";}
-				 return taskStatus[d.status];
+				 var theEventCount = eventStyleCount();
+				 var styleId = d.status%theEventCount;
+				 if(eventStyles[styleId] == null){ return "bar";}
+				 return eventStyles[styleId];
 				 })
 			 .attr("y", 0)
 			 .attr("transform", rectTransform)
@@ -109,6 +119,7 @@ d3.gantt = function() {
 
 		 return gantt;
     };
+
     
     gantt.redraw = function(tasks) {
 
@@ -125,8 +136,8 @@ d3.gantt = function() {
          .attr("rx", 5)
          .attr("ry", 5)
 		 .attr("class", function(d){
-			 if(taskStatus[d.status] == null){ return "bar";}
-			 return taskStatus[d.status];
+			 if(eventStyles[d.status%eventStyleCount] == null){ return "bar";}
+			 return eventStyles[d.status%eventStyleCount];
 			 })
 		 .transition()
 		 .attr("y", 0)
@@ -184,10 +195,10 @@ d3.gantt = function() {
 		return gantt;
     };
     
-    gantt.taskStatus = function(value) {
+    gantt.eventStyles = function(value) {
 		if (!arguments.length)
-			return taskStatus;
-		taskStatus = value;
+			return eventStyles;
+		eventStyles = value;
 		return gantt;
     };
 
@@ -212,7 +223,103 @@ d3.gantt = function() {
 		return gantt;
     };
 
+	 gantt.changeTimeDomain = function(timeDomainString) {
+		this.timeDomainString = timeDomainString;
+		switch (timeDomainString) {
 
+			case "5sec":
+				format = "%H:%M:%S";
+				gantt.timeDomain([ d3.time.second.offset(getEndDate(), -5), getEndDate() ]);
+				this.currentViewBeginTime  = d3.time.second.offset(getEndDate(), -5);
+				this.currentViewEndTime = getEndDate();
+				break;
+			case "15sec":
+				format = "%H:%M:%S";
+				gantt.timeDomain([ d3.time.second.offset(getEndDate(), -15), getEndDate() ]);
+				this.currentViewBeginTime  = d3.time.second.offset(getEndDate(), -15);
+				this.currentViewEndTime = getEndDate();
+				break;
+			case "1min":
+				format = "%H:%M:%S";
+				gantt.timeDomain([ d3.time.minute.offset(getEndDate(), -1), getEndDate() ]);
+				this.currentViewBeginTime  = d3.time.minute.offset(getEndDate(), -1);
+				this.currentViewEndTime = getEndDate();
+				break;
+			case "5min":
+				format = "%H:%M:%S";
+				gantt.timeDomain([ d3.time.minute.offset(getEndDate(), -5), getEndDate() ]);
+				this.currentViewBeginTime  = d3.time.minute.offset(getEndDate(), -5);
+				this.currentViewEndTime = getEndDate();
+				break;
+			case "15min":
+				format = "%H:%M:%S";
+				gantt.timeDomain([ d3.time.minute.offset(getEndDate(), -15), getEndDate() ]);
+				this.currentViewBeginTime  = d3.time.minute.offset(getEndDate(), -15);
+				this.currentViewEndTime = getEndDate();
+				break;
+			case "1hr":
+				format = "%H:%M:%S";
+				gantt.timeDomain([ d3.time.hour.offset(getEndDate(), -1), getEndDate() ]);
+				this.currentViewBeginTime  = d3.time.hour.offset(getEndDate(), -1);
+				this.currentViewEndTime = getEndDate();
+				break;
+			case "3hr":
+				format = "%H:%M";
+				gantt.timeDomain([ d3.time.hour.offset(getEndDate(), -3), getEndDate() ]);
+				this.currentViewBeginTime  = d3.time.hour.offset(getEndDate(), -3);
+				this.currentViewEndTime = getEndDate();
+				break;
+			case "6hr":
+				format = "%H:%M";
+				gantt.timeDomain([ d3.time.hour.offset(getEndDate(), -6), getEndDate() ]);
+				this.currentViewBeginTime  = d3.time.hour.offset(getEndDate(), -6);
+				this.currentViewEndTime = getEndDate();
+				break;
+			case "1day":
+				format = "%H:%M";
+				gantt.timeDomain([ d3.time.day.offset(getEndDate(), -1), getEndDate() ]);
+				this.currentViewBeginTime  = d3.time.day.offset(getEndDate(), -1);
+				this.currentViewEndTime = getEndDate();
+				break;
+			default:
+				format = "%H:%M"
+
+		}
+		this.tickFormat(format);
+		this.redraw(tasks);
+	}
+
+	gantt.panView = function(direction){
+		// gets the length in MS of 50% of the current view. This will be used to determine how far to pan in a single click
+		var shiftTimeLength = ((this.currentViewEndTime - this.currentViewBeginTime) / 2);
+		if(direction === "left"){
+			shiftTimeLength = -shiftTimeLength;
+		}
+		// convert our shift length to Sec and offset our current view start/end times. Once shifted, update the current view to these new times
+		var newStartTime =  d3.time.second.offset(this.currentViewBeginTime, (shiftTimeLength/1000));
+		var newEndTime = d3.time.second.offset(this.currentViewEndTime, (shiftTimeLength/1000));
+		this.timeDomain([ newStartTime , newEndTime ]);
+		this.currentViewBeginTime = newStartTime;
+		this.currentViewEndTime = newEndTime;
+		this.redraw(tasks);
+
+	}
+
+	//function panView(){
+	//	return 0;
+	//}
+
+	function getStartDate() {
+		return this.minDate;
+	}
+
+	function getEndDate() {
+		var lastEndDate = Date.now();
+		if (tasks.length > 0) {
+			lastEndDate = tasks[tasks.length - 1].endDate;
+		}
+		return lastEndDate;
+	}
     
     return gantt;
 };
